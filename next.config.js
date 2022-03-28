e62e1f09e9cded577ab318be416d9c50ecb65669
Rudy B <btte.rudy@gmail.com>
2022-03-28 23:46:15 +0200

    🩹Fix performance"

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         domains: ["upload.wikimedia.org", "flagcdn.com"],
+        dangerouslyAllowSVG: true,
+        contentSecurityPolicy:
+            "default-src 'self'; script-src 'none'; sandbox;",
     },
     i18n: {
         locales: ["en"],
